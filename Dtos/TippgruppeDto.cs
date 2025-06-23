3d0d4ee72b29c6e09a5298b1aab1d1ded7b6b196
Mike Specht <Mike.Specht@gmx.de>
2025-06-23 17:24:44 +0200

    added an AssemblyName for the Project Added two Dto Files

---
+++ Dtos/TippgruppeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TippPlattformMaui.Shared.Dtos;
+
+public class TippGruppeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string SportArt { get; set; }
+    public int AnzahlSpiele { get; set; }
+    public DateTime? Tipptermin { get; set; }
+    public bool IstAdmin { get; set; }
+}
