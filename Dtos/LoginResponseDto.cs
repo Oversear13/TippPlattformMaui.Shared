989ce1d86906affdb99872ba7f456ba2cba124cb
Oversear13 <mike.specht@gmx.de>
2025-07-11 01:10:27 +0200

    Dto's relevant für Profilverwaltung hinzugefügt

--- Dtos/LoginResponseDto.cs
+++ Dtos/LoginResponseDto.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace TippPlattformMaui.Shared.Dtos
 {
-    public class UserDto
+    public class LoginResponseDto
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
         public DateTime? BirthDate { get; set; }
-        /// <summary>
-        /// Neues Passwort (wenn leer oder null, wird im Backend ignoriert)
-        /// </summary>
-        public string? Password { get; set; }
-        public string? CurrentPassword { get; set; }
     }
 }
