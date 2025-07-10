530b6bbbfb59e7db2b3c630a156c8fdac966e030
Mike Specht <Mike.Specht@gmx.de>
2025-07-10 16:25:15 +0200

    Dto's für die Profilverwaltung

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TippPlattformMaui.Shared.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime? BirthDate { get; set; }
+        /// <summary>
+        /// Neues Passwort (wenn leer oder null, wird im Backend ignoriert)
+        /// </summary>
+        public string? Password { get; set; }
+    }
+}
