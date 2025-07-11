6d4eae51511d4bec72e581bf277f8ac598bbb243
Jirapat <86919607+builtttt@users.noreply.github.com>
2025-07-11 11:56:01 +0200

    DTO für Benachrichtigung

---
+++ Dtos/TippErinnerungDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TippPlattform.Controllers.Api
+{
+    public class TippErinnerungDto
+    {
+        public string? SpielBezeichnung { get; set; } // z.B. TeamA vs TeamB
+        public string? TippgruppeName { get; set; } // Name der Tippgruppe
+        public DateTime? Datum { get; set; } // Spielbeginn
+    }
+}
