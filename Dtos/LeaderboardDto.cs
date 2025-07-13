9e59a27a242bc7786581c7e2102be1f7a2958e18
Jirapat <86919607+builtttt@users.noreply.github.com>
2025-07-14 01:25:11 +0200

    app leaderboard

---
+++ Dtos/LeaderboardDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TippPlattformMaui.Shared.Dtos
+{
+
+
+    public class LeaderboardDto
+    {
+        public int Platz { get; set; }
+        public string Name { get; set; }
+        public int Punkte { get; set; }
+    }
+}
